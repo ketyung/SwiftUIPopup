24cc8eab706a2a2996bf016f2e62f057b14919cc
Christopher Chee <ketyung@techchee.com>
2020-12-13 14:57:19 +0800

    Initial Commit

---
+++ Shared/SwiftUIPopupApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPopupApp.swift
+//  Shared
+//
+//  Created by Chee Ket Yung on 13/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPopupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
